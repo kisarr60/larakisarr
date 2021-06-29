7de9e69af85417b1acd332de75ce7786d18ccf80
Ibrahima Sarr <63787826+kisarr60@users.noreply.github.com>
2021-06-29 09:42:14 +0000

    Update Message.php
    I add a new forwquard mess.

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,3 +8,5 @@ class Message extends Model
 {
     protected $fillable = ['name', 'email', 'message'];
 }
+//Define the user function to send a message
+// You might also fix a value for the message.
